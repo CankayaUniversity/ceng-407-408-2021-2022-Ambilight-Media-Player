7ada23642207355d6c7fd75e60520af367c27605
Emre Can Aşık <93338158+heisanengineer@users.noreply.github.com>
2022-03-30 01:37:44 +0300

    This code trying to send different color codes.

---
+++ test/testDifferentColor.py
@@ -0,0 +1,27 @@
+#It is tested by sending different rgb codes to the lamp
+from yeelight import Bulb
+import time
+
+bulb = Bulb('192.168.1.100', effect="smooth")
+bulb.turn_on()
+bulb.set_brightness(100)
+
+getMusicMode=bulb.get_properties()
+if getMusicMode['music_on']=='0':
+    bulb.start_music(55443)
+else:
+    bulb.stop_music()
+    time.sleep(1)
+    bulb.start_music(55443)
+while bulb.music_mode:
+    for r in range(0,255,5):
+        for g in range(0,255,5):
+            for b in range(0,255,5):
+                bulb.set_rgb(r,g,b)
+                time.sleep(0.1)
+            time.sleep(0.1)
+        time.sleep(0.1)
+                
+            
+
+    

1bf4429ba9d7585664d98246da11bc52a87e76a7
Ayhan ARICI <80165784+ayhanarici@users.noreply.github.com>
2022-05-24 14:31:01 +0300

    Add files via upload
    v17

--- src/Video.py
+++ src/Video.py
@@ -1,54 +1,67 @@
 from PyQt5.QtGui import QColor, qGray, QImage, QPainter, QPalette
-from PyQt5.QtMultimedia import (QAbstractVideoBuffer, QMediaContent, QMediaMetaData, QMediaPlayer, QMediaPlaylist, QVideoFrame, QVideoProbe)
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog, QFileDialog, QFormLayout, QHBoxLayout, QLabel, QListView, QMessageBox, QPushButton,
-        QSizePolicy, QSlider, QStyle, QToolButton, QVBoxLayout, QWidget)
-from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtWidgets import QSizePolicy
+from PyQt5.QtCore import Qt
 
 class VideoWidget(QVideoWidget):
+    mPlayer=None
     def __init__(self, parent=None):
         super(VideoWidget, self).__init__(parent)
-        self.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
-        plt = self.palette()
-        plt.setColor(QPalette.Window, Qt.black)
-        self.setPalette(plt)
-        self.setAttribute(Qt.WA_OpaquePaintEvent,False)
-
+        self.setAutoFillBackground(False)
+        self.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.setAttribute(Qt.WA_PaintOnScreen, True)
+        palette = self.palette()
+        palette.setColor(QPalette.Background, Qt.black)
+        self.setPalette(palette)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+       
+        '''
+        background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(40, 44, 51, 255), stop:1 rgba(76, 47, 50, 255));
+        '''
+    def setMediaPlayer(self, mp):
+        self.mPlayer=mp   
+        
     def keyPressEvent(self, event):
         #Set keyPressEvent
         if event.key() == Qt.Key_Escape and self.isFullScreen():
             self.setFullScreen(False)
             self.setCursor(Qt.ArrowCursor)
             event.accept()
+            
         elif event.key() == Qt.Key_F:
-            if self.mediaObject().state() == QMediaPlayer.PlayingState:
+            if self.mPlayer.state() == 1:
                 self.setFullScreen(not self.isFullScreen())
                 if self.isFullScreen():
                     self.setCursor(Qt.BlankCursor)
                 else:
                     self.setCursor(Qt.ArrowCursor)
-            event.accept()
-        elif event.key() == Qt.Key_Space:
-            if self.mediaObject().state() == QMediaPlayer.PlayingState:
-                self.mediaObject().pause()
             else:
-                self.mediaObject().play()
+                pass
+            event.accept()
+            
+        elif event.key() == Qt.Key_Space:
+            if self.mPlayer.state() == 1:
+                self.mPlayer.pause()
+            else:
+                self.mPlayer.play()
             event.accept()
         elif event.key() == Qt.Key_M:
-            self.mediaObject().setMuted(not self.mediaObject().isMuted())
+            self.mPlayer.setMuted(not self.mPlayer.isMuted())
             event.accept()
+            
         else:
             super(VideoWidget, self).keyPressEvent(event)
-            
+           
     def mouseDoubleClickEvent(self, event):
         #Set mouseDoubleClickEvent
         try:
-            if self.mediaObject().state() == QMediaPlayer.PlayingState:
+            if self.mPlayer.state() == 1:
                 self.setFullScreen(not self.isFullScreen())
                 if self.isFullScreen():
                     self.setCursor(Qt.BlankCursor)
                 else:
                     self.setCursor(Qt.ArrowCursor)
-            event.accept()
+                event.accept()
         except:
             pass
+    
--- src/VideoGrabber.py
+++ src/VideoGrabber.py
@@ -1,84 +1,69 @@
-from PyQt5.QtCore import QDir, Qt, QUrl, pyqtSignal, QPoint, QRect, QObject
-from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer, QVideoFrame, QAbstractVideoSurface, QAbstractVideoBuffer, QVideoSurfaceFormat
+from PyQt5.QtWidgets import QPushButton, QStyle, QVBoxLayout, QWidget, QFileDialog, QLabel, QSlider, QHBoxLayout
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QApplication, QFileDialog, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget, QMainWindow,QWidget, QPushButton, QAction
-from PyQt5.QtGui import QIcon, QPainter, QImage
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtMultimedia import *
+from PyQt5.QtMultimediaWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
+import os
 
-class VideoFrameGrabber(QAbstractVideoSurface):
-    frameAvailable = pyqtSignal(QImage)
-    def __init__(self, widget: QWidget, parent: QObject):
+class GrabVideoSurface(QAbstractVideoSurface):
+    def __init__(self, parent=None):
         super().__init__(parent)
-        self.widget = widget
-    def supportedPixelFormats(self, handleType):
-        return [QVideoFrame.Format_ARGB32, QVideoFrame.Format_ARGB32_Premultiplied,
-                QVideoFrame.Format_RGB32, QVideoFrame.Format_RGB24, QVideoFrame.Format_RGB565,
-                QVideoFrame.Format_RGB555, QVideoFrame.Format_ARGB8565_Premultiplied,
-                QVideoFrame.Format_BGRA32, QVideoFrame.Format_BGRA32_Premultiplied, QVideoFrame.Format_BGR32,
-                QVideoFrame.Format_BGR24, QVideoFrame.Format_BGR565, QVideoFrame.Format_BGR555,
-                QVideoFrame.Format_BGRA5658_Premultiplied, QVideoFrame.Format_AYUV444,
-                QVideoFrame.Format_AYUV444_Premultiplied, QVideoFrame.Format_YUV444,
-                QVideoFrame.Format_YUV420P, QVideoFrame.Format_YV12, QVideoFrame.Format_UYVY,
-                QVideoFrame.Format_YUYV, QVideoFrame.Format_NV12, QVideoFrame.Format_NV21,
-                QVideoFrame.Format_IMC1, QVideoFrame.Format_IMC2, QVideoFrame.Format_IMC3,
-                QVideoFrame.Format_IMC4, QVideoFrame.Format_Y8, QVideoFrame.Format_Y16,
-                QVideoFrame.Format_Jpeg, QVideoFrame.Format_CameraRaw, QVideoFrame.Format_AdobeDng]
+        self.currentFrame = QImage()
+    @property
+    def getCurrentFrame(self):
+        return self.currentFrame
 
-    def isFormatSupported(self, format):
-        imageFormat = QVideoFrame.imageFormatFromPixelFormat(format.pixelFormat())
-        size = format.frameSize()
-        return imageFormat != QImage.Format_Invalid and not size.isEmpty() and format.handleType() == QAbstractVideoBuffer.NoHandle
-
-    def start(self, format: QVideoSurfaceFormat):
-        imageFormat = QVideoFrame.imageFormatFromPixelFormat(format.pixelFormat())
-        size = format.frameSize()
-        if imageFormat != QImage.Format_Invalid and not size.isEmpty():
-            self.imageFormat = imageFormat
-            self.imageSize = size
-            self.sourceRect = format.viewport()
-            super().start(format)
-            self.widget.updateGeometry()
-            self.updateVideoRect()
-            return True
-        else:
-            return False
-    def stop(self):
-        self.currentFrame = QVideoFrame()
-        self.targetRect = QRect()
-        self.widget.update()
+    def supportedPixelFormats(self, handleType=QAbstractVideoBuffer.NoHandle):
+        formats = [QVideoFrame.PixelFormat()]
+        if handleType == QAbstractVideoBuffer.NoHandle:
+            for f in [
+                QVideoFrame.Format_RGB32,
+                QVideoFrame.Format_ARGB32,
+                QVideoFrame.Format_ARGB32_Premultiplied,
+                QVideoFrame.Format_RGB565,
+                QVideoFrame.Format_RGB555,
+                QVideoFrame.Format_ARGB32,
+                QVideoFrame.Format_ARGB32_Premultiplied,
+                QVideoFrame.Format_RGB32,
+                QVideoFrame.Format_RGB24,
+                QVideoFrame.Format_RGB565,
+                QVideoFrame.Format_RGB555,
+                QVideoFrame.Format_ARGB8565_Premultiplied,
+                QVideoFrame.Format_BGRA32,
+                QVideoFrame.Format_BGRA32_Premultiplied,
+                QVideoFrame.Format_BGR32,
+                QVideoFrame.Format_BGR24,
+                QVideoFrame.Format_BGR565,
+                QVideoFrame.Format_BGR555,
+                QVideoFrame.Format_BGRA5658_Premultiplied,
+                QVideoFrame.Format_AYUV444,
+                QVideoFrame.Format_AYUV444_Premultiplied,
+                QVideoFrame.Format_YUV444,
+                QVideoFrame.Format_YUV420P,
+                QVideoFrame.Format_YV12,
+                QVideoFrame.Format_UYVY,
+                QVideoFrame.Format_YUYV,
+                QVideoFrame.Format_NV12,
+                QVideoFrame.Format_NV21,
+                QVideoFrame.Format_IMC1,
+                QVideoFrame.Format_IMC2,
+                QVideoFrame.Format_IMC3,
+                QVideoFrame.Format_IMC4,
+                QVideoFrame.Format_Y8,
+                QVideoFrame.Format_Y16,
+                QVideoFrame.Format_Jpeg,
+                QVideoFrame.Format_CameraRaw,
+                QVideoFrame.Format_AdobeDng,
+            ]:
+                formats.append(f)
+        return formats
 
     def present(self, frame):
-        if frame.isValid():
-            cloneFrame = QVideoFrame(frame)
-            cloneFrame.map(QAbstractVideoBuffer.ReadOnly)
-            image = QImage(cloneFrame.bits(), cloneFrame.width(), cloneFrame.height(),
-                           QVideoFrame.imageFormatFromPixelFormat(cloneFrame.pixelFormat()))
-            self.frameAvailable.emit(image)
-            cloneFrame.unmap()
-        if self.surfaceFormat().pixelFormat() != frame.pixelFormat() or self.surfaceFormat().frameSize() != frame.size():
-            self.setError(QAbstractVideoSurface.IncorrectFormatError)
-            return False
-        else:
-            self.currentFrame = frame
-            self.widget.repaint(self.targetRect)
-            return True
-    def updateVideoRect(self):
-        size = self.surfaceFormat().sizeHint()
-        size.scale(self.widget.size().boundedTo(size), Qt.KeepAspectRatio)
-
-        self.targetRect = QRect(QPoint(0, 0), size)
-        self.targetRect.moveCenter(self.widget.rect().center())
-
-    def paint(self, painter):
-        if self.currentFrame.map(QAbstractVideoBuffer.ReadOnly):
-            oldTransform = self.painter.transform()
-
-        if self.surfaceFormat().scanLineDirection() == QVideoSurfaceFormat.BottomToTop:
-            self.painter.scale(1, -1)
-            self.painter.translate(0, -self.widget.height())
-
-        image = QImage(self.currentFrame.bits(), self.currentFrame.width(), self.currentFrame.height(),
-                       self.currentFrame.bytesPerLine(), self.imageFormat)
-
-        self.painter.drawImage(self.targetRect, image, self.sourceRect)
-        self.painter.setTransform(oldTransform)
-        self.currentFrame.unmap()
+        self.currentFrame = frame.image()
+        return True
+    
